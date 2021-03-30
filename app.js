b274b981e7bb923519d92b39321da9028c7d2320
Aditya Malik <aditya007@outlook.com>
2021-03-30 21:40:22 +0530

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv/config");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = "3000" || process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_STRING, {
