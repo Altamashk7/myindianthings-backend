14b73e8d392597a3959f7673657ffcb01940ad95
Altamash Khan <altamashkhan59@gmail.com>
2021-04-12 22:03:34 +0530

    order delete bug solved

--- routers/orders.js
+++ routers/orders.js
@@ -59,25 +59,16 @@ router.put("/:id", async (req, res) => {
   res.send(order);
 });
 
-router.delete("/:id", (req, res) => {
-  Order.findByIdAndRemove(req.params.id)
-    .then(async (order) => {
-      if (order) {
-        await order.orderItems.map(async (orderItem) => {
-          await OrderItem.findByIdAndRemove(orderItem);
-        });
-        return res
-          .status(200)
-          .json({ success: true, message: "the order is deleted!" });
-      } else {
-        return res
-          .status(404)
-          .json({ success: false, message: "order not found!" });
-      }
-    })
-    .catch((err) => {
-      return res.status(500).json({ success: false, error: err });
-    });
+router.delete("/:id", async (req, res) => {
+  try {
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (!order) {
+      return res.status(404).send();
+    }
+    res.send(order);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 router.get("/get/totalsales", async (req, res) => {
